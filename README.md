68854517fcf78d4fce1b3d6c005524352d7a44f8
Bpbot33 <89935732+Bpbot33@users.noreply.github.com>
2022-09-07 09:34:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-ablhvu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-ablhvu)
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './main.css';
+import Expenses from './routes/expenses.jsx';
+import Invoices from './routes/invoices';
+
+let rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+<BrowserRouter>
+  <App />
+</BrowserRouter>;
